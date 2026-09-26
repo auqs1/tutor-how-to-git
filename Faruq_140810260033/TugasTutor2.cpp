a04b7405d7e89e4bba7cb33cbfd4f28d6c5020ed
al faruq <you@example.com>
2026-09-26 21:53:11 +0700

    Simulasi Mesin Minuman dengan menggunakan Switch dan If-else

--- Faruq_140810260033/TugasTutor2.cpp
+++ Faruq_140810260033/TugasTutor2.cpp
@@ -7,7 +7,9 @@ int main(){
     int sisaUang;;
     int kembalian;
     int pecahan5ribu;
-    
+    int pecahan1ribu;
+    int sisaKembalian;
+
     std::cout << "=== VENDING MACHINE AUTOMATIC ===" << std::endl <<
     "Pilihan Minuman:" << std::endl << "1. Air Mineral (Rp 4000)" << 
     std:: endl << "2. Teh Botol (6000)" << std::endl << "3. Kopi Susu (Rp 10000)"
@@ -15,36 +17,46 @@ int main(){
     
     std::cout << "Pilih Kode Minuman (1-3): ";
     std::cin >> angka; 
-    std::cout << std::endl << "Masukkan Uang Anda (Rp): ";
-    std::cin >> duit;
+
 
     switch (angka){
         case 1:
             harga = 4000;
-    break;
+            break;
         case 2:
              harga = 6000;
-    break;
+            break;
         case 3: 
             harga = 10000;
+            break;
     default:
     std::cout << "Kode Tidak Valid.";
-    break;
     }
+
+    if (angka < 4){
+    std::cout << std::endl << "Masukkan Uang Anda (Rp): ";
+    std::cin >> duit;
+    
+
 
     if(duit < harga){
         sisaUang = harga - duit;
-        std::cout << "Uang anda kurang" << std::endl;
-        std::cout << "Sisa uang : Rp" << sisaUang;
+        std::cout << "Uang anda kurang." << std::endl;
+        std::cout << "Sisa kekurangan uang : Rp " << sisaUang;
     }else{
         kembalian = duit - harga;
-        std::cout << "Kembalian anda : Rp" << kembalian;
+        std::cout << "Kembalian anda : Rp" << kembalian << std::endl;
         
-        pecahan5ribu = duit / 5000;
-        pecahan1ribu = 
+        pecahan5ribu = kembalian / 5000;
+        std::cout << pecahan5ribu << " Lembar" << std::endl;
+
+        sisaKembalian = kembalian % 5000;
         
+        pecahan1ribu = sisaKembalian / 1000;
+        std::cout << pecahan1ribu << " Lembar";
        
     }
-    
+}else
+
    return 0;
 }
